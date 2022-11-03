ec5a696dcde1ab6337cf20f6490e1575f01e7279
nurulaist <nurulainiistiqomah32@gmail.com>
2022-11-03 16:05:09 +0700

    baru sampe ke pindah home page

--- tugas/gen-11-tugas-react-redux-authentication-nurul/src/Home.jsx
+++ tugas/gen-11-tugas-react-redux-authentication-nurul/src/Home.jsx
@@ -3,14 +3,13 @@ import { logout } from "./authSlice";
 
 export default function Home() {
   const authSlice = useSelector((state) => state.auth);
-  const dispatch = useDispatch();
 
   return (
     <>
       <h1>Ini Home</h1>
       <br /> <br />
-      <h2> Welcome to,</h2>
-      <p>Ini token anda: </p>
+      <h2> Welcome to, {authSlice.userData.firstName}</h2>
+      <p>Ini token anda: {authSlice.token}</p>
       <br />
       <br />
       <button>Logout</button>
